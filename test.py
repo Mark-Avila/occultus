ea7266f8f87fde954af1ab514ce744c72507c250
Mark Avila <avilamark96@gmail.com>
2023-11-21 14:06:51 +0800

    quick commit

--- test.py
+++ test.py
@@ -2,17 +2,10 @@ from occultus.core_temp import Occultus
 
 detect = Occultus("weights/kamukha-v2.pt")
 detect.load_video("video/pips-2.mp4")
-# detect.load_stream()
+detect.load_stream()
 detect.set_config({"conf-thres": 0.5, "flipped": False, "nolabel": True})
 
-# frames = detect.initialize_model()
+frames = detect.initialize_model()
 
-# for pred, dataset, iterables in detect.run_inference(frames):
-#     detect.process_preds(pred, dataset, iterables)
-
-detect.run()
-
-# detect.run_inference(frames)
-
-# detect.append_id(1)
-# detect.run()
+for pred, dataset, iterables in detect.run_inference(frames):
+    detect.process_preds(pred, dataset, iterables)
