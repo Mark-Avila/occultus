79fd94c1ed78fe279dbfb61da60850e329511934
Mark Avila <avilamark96@gmail.com>
2023-11-21 21:28:31 +0800

    fix: renamed files

--- test.py
+++ test.py
@@ -1,27 +1,22 @@
-from occultus.core_temp import Occultus
+from occultus.core import Occultus
 from tqdm import tqdm
 
 detect = Occultus("weights/kamukha-v2.pt")
-detect.load_video("video/pips-2.mp4")
-# detect.load_stream()
+# detect.load_video("video/pips-2.mp4")
+detect.load_stream()
 detect.set_config({"conf-thres": 0.5, "flipped": False, "nolabel": True})
 
 frames = detect.initialize()
 
-# Assuming you know the total number of iterations beforehand
-total_iterations = frames.numframes()
+# Assuming you know the total number of iterations beforehand``
+# total_iterations = frames.numframes()
 
-progress_bar = tqdm(
-    total=total_iterations, desc="Blurring frames", position=0, leave=True
-)
+# progress_bar = tqdm(
+#     total=total_iterations, desc="Blurring frames", position=0, leave=True
+# )
 
 for pred, dataset, iterables in detect.inference(frames):
     frame = detect.process(pred, dataset, iterables)
 
-    detect.save_video(frame, iterables)
-
-    # Update the progress bar
-    progress_bar.update(1)
-
-# Close the progress bar
-progress_bar.close()
+    # detect.save_video(frame, iterables)
+    detect.show_frame(frame)
